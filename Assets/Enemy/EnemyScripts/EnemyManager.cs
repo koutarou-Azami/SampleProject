d574471476c2633c5dc2120e7a7ca0c8992ad212
koutarou-Azami <reica36@gmail.com>
2017-11-24 13:49:54 +0900

    更新

--- Assets/Enemy/EnemyScripts/EnemyManager.cs
+++ Assets/Enemy/EnemyScripts/EnemyManager.cs
@@ -7,7 +7,6 @@ public class EnemyManager : MonoBehaviour
 
     public GameObject[] _Enemy;
 
-    public GameObject[] _enemyCount;
     public int _instantiateEnemy;
 
     public int _timer;
@@ -30,35 +29,34 @@ public class EnemyManager : MonoBehaviour
         _timer++;
         if (_timer >= 300.0f)
         {
-            Check("Enemy");
+            Check();
         }
     }
 
     // シーン内にある任意のタグのオブジェクトの数を数える
-    public void Check(string tagName)
+    public void Check()
     {
-        _enemyCount = GameObject.FindGameObjectsWithTag(tagName);
-        if (_enemyCount.Length <= 0)
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        int enemyCount = enemies.Length;
+        
+        if (enemyCount > 0)
+            return;
+
+        for (int i = 0; i < _instantiateEnemy; i++)
         {
-            _timer = 0;
             EnemyInstantiate();
         }
+        _timer = 0;
     }
 
     public void EnemyInstantiate()
     {
-        for(int i = 0; i < _instantiateEnemy; ++i)
-        {
-            if (_enemyCount.Length <= _instantiateEnemy)
-            {
-                // エネミーオブジェクトの配列内からランダムで1種類選出
-                GameObject element = _Enemy[Random.Range(0, _Enemy.Length)];
-                // 座標位置をランダムで決める
-                float x = Random.Range(-100.0f, 100.0f);
-                float z = Random.Range(-100.0f, 100.0f);
-                // エネミーオブジェクトを生成
-                GameObject.Instantiate(element, new Vector3(x, element.transform.position.y, z), Quaternion.identity);
-            }
-        }
+        // エネミーオブジェクトの配列内からランダムで1種類選出
+        GameObject element = _Enemy[Random.Range(0, _Enemy.Length)];
+        // 座標位置をランダムで決める
+        float x = Random.Range(-100.0f, 100.0f);
+        float z = Random.Range(-100.0f, 100.0f);
+        // エネミーオブジェクトを生成
+        GameObject.Instantiate(element, new Vector3(x, element.transform.position.y, z), Quaternion.identity);
     }
 }
